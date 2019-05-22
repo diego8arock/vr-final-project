0733185a9f1c9e3fde758e0312ed2fc02428ed5c
diego8arock <diegoa2000alt@gmail.com>
2019-05-21 20:24:35 -0500

    Agregar modelos

--- stations/election/election.gd
+++ stations/election/election.gd
@@ -1,24 +1,43 @@
 extends StaticBody
 
 
+enum CORRECT {LEFT, CENTER, RIGHT}
+
 #Tables
 onready var left_table = $LeftTable
 onready var right_table = $RightTable
 onready var center_table = $CenterTable
 
+var correct_left_table : bool = false
+var correct_right_table : bool = false
+var correct_center_table : bool = false
+
 #Callenge 1
-var c1o1_umbrella : PackedScene = preload("res://stations/election/option/umbrella/Umbrella.tscn")
-var c1o2_shoe : PackedScene = preload("res://stations/election/option/shoe/Shoe.tscn")
-var c1o3_bird
+var umbrella : PackedScene = preload("res://stations/election/option/umbrella/Umbrella.tscn")
+var shoe : PackedScene = preload("res://stations/election/option/shoe/Shoe.tscn")
+var chicken : PackedScene = preload("res://assets/chicken/chicken.tscn")
 
-func _ready() -> void:
-	_start_challenge_1()
-	pass # Replace with function body.
+var fish : PackedScene = preload("res://assets/fish/Fish.tscn")
 
 
-func _start_challenge_1() -> void:	
-	left_table.set_option_model(c1o1_umbrella.instance(), "umbrella", true)
-	right_table.set_option_model(c1o2_shoe.instance(), "shoe", false)
+var extinguisher : PackedScene = preload("res://assets/extinguisher/Estinguisher.tscn")
 
+func start_rain_challenge(correct) -> void:
+	left_table.set_option_model(umbrella.instance(), "umbrella", correct_left_table)
+	right_table.set_option_model(shoe.instance(), "shoe", correct_right_table)
+	center_table.set_option_model(chicken.instance(), "chicken", correct_center_table)
 
+func start_cat_challenge(correct) -> void:
+	right_table.set_option_model(fish.instance(), "shoe", correct_right_table)
+	
+func start_fire_challenge(correct) -> void:
+	center_table.set_option_model(extinguisher.instance(), "chicken", correct_center_table)
 
+func set_correct_option(correct) -> void:
+	match CORRECT:
+		CORRECT.LEFT:
+			correct_left_table = true
+		CORRECT.CENTER:
+			correct_center_table = true
+		CORRECT.RIGHT:
+			correct_right_table = true
--- world/room/room.gd
+++ world/room/room.gd
@@ -3,12 +3,18 @@ extends StaticBody
 enum CHALLENGE {FIRE, RAIN, CAT}
 export (CHALLENGE) var challenge 
 
+enum CORRECT {LEFT, CENTER, RIGHT}
+export (CORRECT) var correct_answer
+
 func _ready() -> void:
 	match challenge:
 		CHALLENGE.FIRE:
 			$Challenge.update_challenge(CHALLENGE.FIRE)
+			$Election.start_fire_challenge(correct_answer)
 		CHALLENGE.RAIN:
 			$Challenge.update_challenge(CHALLENGE.RAIN)
+			$Election.start_rain_challenge(correct_answer)
 		CHALLENGE.CAT:
 			$Challenge.update_challenge(CHALLENGE.CAT)
+			$Election.start_cat_challenge(correct_answer)
 
